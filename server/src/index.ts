04c9e716f3edd38e05722dc7c072d38dd410ba3d
Nikki <nikhilpareeshwad@gmail.com>
2026-04-18 11:57:36 +0530

    add typescript support

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import resumeRoutes from "./routes/resume.routes.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running 🚀");
+});
+
+
+//ROUTES
+app.use("/api/resume", resumeRoutes);
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
