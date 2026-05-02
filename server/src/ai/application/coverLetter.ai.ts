7deaa8b6983069c9e3271d784adbf493491c97da
Nikki <nikhilpareeshwad@gmail.com>
2026-05-02 11:45:17 +0530

    add cover letter Prompt for openai system prompt

---
+++ server/src/ai/application/coverLetter.ai.ts
@@ -0,0 +1,57 @@
+import { openai } from "../core/openai.js"
+
+export const generateCoverLetter = async (resumeText: string, jobDescription: string, explanation: any) => {
+
+
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            temperature: 0.7,
+            messages: [
+                {
+
+                    role: "system",
+                    content: `You are a professional career assistant. 
+                    
+                    Write a personalised cover letter. 
+                    
+                    Use:
+                   - candidate strengths
+                   - job requirements
+                   - address gaps subtly
+
+                    Rules:
+                    - be concise
+                    - professional tone
+                    - no generic phrases
+                    - no placeholders
+
+`
+                },
+                {
+                    role: "user",
+                    content: `
+                            Resume:
+                            ${resumeText}
+
+                            Job Description:
+                            ${jobDescription}
+
+                            Strengths:
+                            ${explanation.strengths}
+
+                            Gaps:
+                            ${explanation.gaps}`
+                }
+            ]
+
+
+        })
+
+        return response.choices[0].message?.content || "";
+
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
